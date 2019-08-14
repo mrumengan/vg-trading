5b780cb5056ef2a9e2109bf98eb60e735dbeb31e
Legion\Mason Rumengan <mrumengan@taptalents.com>
2019-08-14 13:20:16 +0700

    update

---
+++ header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>" />
+    <link rel="profile" href="http://gmpg.org/xfn/11" />
+    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+    <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
+    <?php wp_head(); ?>
+</head>
