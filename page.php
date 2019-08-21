f34077e5c782a7c40e3de0611dce136b4cb9e512
Legion\Mason Rumengan <mrumengan@taptalents.com>
2019-08-21 15:53:47 +0700

    [update]

--- page.php
+++ page.php
@@ -1,9 +1,12 @@
 <?php
 get_header();
+
 if ( have_posts() ) :
     while ( have_posts() ) : the_post();
-        // Display post content
+        the_content();
     endwhile;
+else :
+    _e( 'Sorry, no posts matched your criteria.', 'textdomain' );
 endif;
 
 get_sidebar();
